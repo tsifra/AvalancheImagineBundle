ea8da58f9bfe8e819563a0620fe90316a9de70db
tsifra <asafaaa040@ln360.ru>
2026-05-06 20:14:51 +0000

    Update AvalancheImagineExtension.php

--- DependencyInjection/AvalancheImagineExtension.php
+++ DependencyInjection/AvalancheImagineExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class AvalancheImagineExtension extends Extension
 {
